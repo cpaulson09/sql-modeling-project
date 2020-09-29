b84ce1f5f90aafd591fc8979a16b763747529e50
Gracias <gclaude@insiderealestate.com>
2020-09-28 19:48:38 -0600

    removed mkdir

---
+++ DAO/skeleton.js
@@ -0,0 +1,21 @@
+create(v) { //v is a vendor object
+  // no return
+}
+  
+  read(id) { //id is the key of a vendor
+  // return vendor object
+}
+  
+  update(v) { //v is a vendor object
+  // replace existing row(s)
+  // no return
+}
+  
+  // renamed to remove because delete is a JS keyword
+  remove(id) { //id is the key of a vendor
+  // no return
+}
+  
+  list() { //no params
+  // return array of vendor objects
+}
