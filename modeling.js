7d985017589e3ec2d9c02a93d3cfa3a4e351ab8f
Nate Back <nkback@gmail.com>
2020-09-29 21:56:16 -0600

    Add db connection in modeling.js and pass as argument.

--- modeling.js
+++ modeling.js
@@ -1,111 +1,443 @@
-const faker = require('faker')
-employee2DAO = require('../sql-modeling-project/sqliteDAO/ORM2/employeeDAO2Sqlite')
+const faker = require("faker");
+employee2DAO = require("../sql-modeling-project/sqliteDAO/ORM2/employeeDAO2Sqlite");
+customer2DAO = require("../sql-modeling-project/sqliteDAO/ORM2/customerDAO2Sqlite");
+const sqlite = require("sqlite3").verbose();
 
 // 1 - create ES6 classes with Contructors for Person, Employee, Manager, Executive, Nonemployee, Contractor, Vendor, Customer
 // parent class
 class Person {
-    constructor(id, firstName, middleName, lastName, dob, phone, email, streetAddress, city, state, zip){
-        this.id = id
-        this.firstName = firstName
-        this.middleName = middleName
-        this.lastName = lastName
-        this.dob = dob
-        this.phone = phone
-        this.email = email
-        this.streetAddress = streetAddress
-        this.city = city
-        this.state = state
-        this.zip = zip
+    constructor(
+        id,
+        firstName,
+        middleName,
+        lastName,
+        dob,
+        phone,
+        email,
+        streetAddress,
+        city,
+        state,
+        zip
+    ) {
+        this.id = id;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.dob = dob;
+        this.phone = phone;
+        this.email = email;
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
     }
 }
 class Employee extends Person {
-    constructor (id, personId, firstName, middleName, lastName, dob, phone, email, streetAddress, city, state, zip, companyId, department, title, salary, managerId) {
-        super(id, firstName, middleName, lastName, dob, phone, email, streetAddress, city, state, zip)
-        this.personId = personId
-        this.companyId = companyId
-        this.department = department
-        this.title = title
-        this.salary = salary
-        this.managerId = managerId
+    constructor(
+        id,
+        personId,
+        firstName,
+        middleName,
+        lastName,
+        dob,
+        phone,
+        email,
+        streetAddress,
+        city,
+        state,
+        zip,
+        companyId,
+        department,
+        title,
+        salary,
+        managerId
+    ) {
+        super(
+            id,
+            firstName,
+            middleName,
+            lastName,
+            dob,
+            phone,
+            email,
+            streetAddress,
+            city,
+            state,
+            zip
+        );
+        this.personId = personId;
+        this.companyId = companyId;
+        this.department = department;
+        this.title = title;
+        this.salary = salary;
+        this.managerId = managerId;
     }
 }
 class Manager extends Employee {
-    constructor (id, employeeId, firstName, middleName, lastName, dob, phone, email, streetAddress, city, state, zip, companyId, department, title, salary, managerId, directReports) {
-        super(id, firstName, middleName, lastName, dob, phone, email, streetAddress, city, state, zip, companyId, department, title, salary, managerId)
-        this.employeeId = employeeId
-        this.directReports = directReports
+    constructor(
+        id,
+        employeeId,
+        firstName,
+        middleName,
+        lastName,
+        dob,
+        phone,
+        email,
+        streetAddress,
+        city,
+        state,
+        zip,
+        companyId,
+        department,
+        title,
+        salary,
+        managerId,
+        directReports
+    ) {
+        super(
+            id,
+            firstName,
+            middleName,
+            lastName,
+            dob,
+            phone,
+            email,
+            streetAddress,
+            city,
+            state,
+            zip,
+            companyId,
+            department,
+            title,
+            salary,
+            managerId
+        );
+        this.employeeId = employeeId;
+        this.directReports = directReports;
     }
 }
 class Executive extends Manager {
-    constructor (id, firstName, middleName, lastName, dob, phone, email, streetAddress, city, state, zip, companyId, department, title, salary, managerId, bonus, directReports) {
-        super(id, firstName, middleName, lastName, dob, phone, email, streetAddress, city, state, zip, companyId, department, title, salary, managerId, directReports)
-        this.bonus = bonus
+    constructor(
+        id,
+        firstName,
+        middleName,
+        lastName,
+        dob,
+        phone,
+        email,
+        streetAddress,
+        city,
+        state,
+        zip,
+        companyId,
+        department,
+        title,
+        salary,
+        managerId,
+        bonus,
+        directReports
+    ) {
+        super(
+            id,
+            firstName,
+            middleName,
+            lastName,
+            dob,
+            phone,
+            email,
+            streetAddress,
+            city,
+            state,
+            zip,
+            companyId,
+            department,
+            title,
+            salary,
+            managerId,
+            directReports
+        );
+        this.bonus = bonus;
     }
 }
-
 
 class Nonemployee extends Person {
-    constructor(id, firstName, middleName, lastName, dob, phone, email, streetAddress, city, state, zip, company){
-        super(id, firstName, middleName, lastName, dob, phone, email, streetAddress, city, state, zip)
-        this.company = company
+    constructor(
+        id,
+        firstName,
+        middleName,
+        lastName,
+        dob,
+        phone,
+        email,
+        streetAddress,
+        city,
+        state,
+        zip,
+        company
+    ) {
+        super(
+            id,
+            firstName,
+            middleName,
+            lastName,
+            dob,
+            phone,
+            email,
+            streetAddress,
+            city,
+            state,
+            zip
+        );
+        this.company = company;
     }
 }
-class Contractor extends Nonemployee{
-    constructor(id, firstName, middleName, lastName, dob, phone, email, streetAddress, city, state, zip, company){
-        super(id, firstName, middleName, lastName, dob, phone, email, streetAddress, city, state, zip, company)
+class Contractor extends Nonemployee {
+    constructor(
+        id,
+        firstName,
+        middleName,
+        lastName,
+        dob,
+        phone,
+        email,
+        streetAddress,
+        city,
+        state,
+        zip,
+        company
+    ) {
+        super(
+            id,
+            firstName,
+            middleName,
+            lastName,
+            dob,
+            phone,
+            email,
+            streetAddress,
+            city,
+            state,
+            zip,
+            company
+        );
     }
 }
-class Vendor extends Nonemployee{
-    constructor(id, firstName, middleName, lastName, dob, phone, email, streetAddress, city, state, zip, company){
-        super(id, firstName, middleName, lastName, dob, phone, email, streetAddress, city, state, zip, company)
+class Vendor extends Nonemployee {
+    constructor(
+        id,
+        firstName,
+        middleName,
+        lastName,
+        dob,
+        phone,
+        email,
+        streetAddress,
+        city,
+        state,
+        zip,
+        company
+    ) {
+        super(
+            id,
+            firstName,
+            middleName,
+            lastName,
+            dob,
+            phone,
+            email,
+            streetAddress,
+            city,
+            state,
+            zip,
+            company
+        );
     }
 }
-class Customer extends Nonemployee{
-    constructor(id, firstName, middleName, lastName, dob, phone, email, streetAddress, city, state, zip, company){
-        super(id, firstName, middleName, lastName, dob, phone, email, streetAddress, city, state, zip, company)
+class Customer extends Nonemployee {
+    constructor(
+        id,
+        firstName,
+        middleName,
+        lastName,
+        dob,
+        phone,
+        email,
+        streetAddress,
+        city,
+        state,
+        zip,
+        company
+    ) {
+        super(
+            id,
+            firstName,
+            middleName,
+            lastName,
+            dob,
+            phone,
+            email,
+            streetAddress,
+            city,
+            state,
+            zip,
+            company
+        );
     }
 }
-
 
 // 5 - put the objects in arrays
-let vendors = []
-let customers = []
-let contractors = []
-let employees = []
-let managers = []
-let executives = []
+let vendors = [];
+let customers = [];
+let contractors = [];
+let employees = [];
+let managers = [];
+let executives = [];
 
 // 2 - use faker.js to create fake data
 // 3 - call the constructors with faker data
-for (let i=0; i<5; i++){
-    vendors.push(new Vendor(faker.random.number(), faker.name.firstName(), faker.name.firstName(), faker.name.lastName(), faker.date.past(), faker.phone.phoneNumber(), faker.internet.email(), faker.address.streetAddress(), faker.address.city(), faker.address.state(), faker.address.zipCode(), faker.company.companyName()))
-    customers.push(new Customer(faker.random.number(), faker.name.firstName(), faker.name.firstName(), faker.name.lastName(), faker.date.past(), faker.phone.phoneNumber(), faker.internet.email(), faker.address.streetAddress(), faker.address.city(), faker.address.state(), faker.address.zipCode(), faker.company.companyName()))
-    contractors.push(new Contractor(faker.random.number(), faker.name.firstName(), faker.name.firstName(), faker.name.lastName(), faker.date.past(), faker.phone.phoneNumber(), faker.internet.email(), faker.address.streetAddress(), faker.address.city(), faker.address.state(), faker.address.zipCode(), faker.company.companyName()))
-    managers.push(new Manager(faker.random.number(), faker.random.number(), faker.name.firstName(), faker.name.firstName(), faker.name.lastName(), faker.date.past(), faker.phone.phoneNumber(), faker.internet.email(), faker.address.streetAddress(), faker.address.city(), faker.address.state(), faker.address.zipCode(), faker.random.number(), faker.commerce.department(), faker.name.jobTitle(), faker.finance.amount(), faker.random.number(), faker.random.number()))
-    if (i < 3){
-        executives.push(new Executive(faker.random.number(),faker.name.firstName(), faker.name.firstName(), faker.name.lastName(), faker.date.past(), faker.phone.phoneNumber(), faker.internet.email(), faker.address.streetAddress(), faker.address.city(), faker.address.state(), faker.address.zipCode(), faker.random.number(), faker.commerce.department(), faker.name.jobTitle(), faker.finance.amount(), faker.random.number(), faker.finance.amount(), faker.random.number()))
+for (let i = 0; i < 5; i++) {
+    vendors.push(
+        new Vendor(
+            faker.random.number(),
+            faker.name.firstName(),
+            faker.name.firstName(),
+            faker.name.lastName(),
+            faker.date.past(),
+            faker.phone.phoneNumber(),
+            faker.internet.email(),
+            faker.address.streetAddress(),
+            faker.address.city(),
+            faker.address.state(),
+            faker.address.zipCode(),
+            faker.company.companyName()
+        )
+    );
+    customers.push(
+        new Customer(
+            faker.random.number(),
+            faker.name.firstName(),
+            faker.name.firstName(),
+            faker.name.lastName(),
+            faker.date.past(),
+            faker.phone.phoneNumber(),
+            faker.internet.email(),
+            faker.address.streetAddress(),
+            faker.address.city(),
+            faker.address.state(),
+            faker.address.zipCode(),
+            faker.company.companyName()
+        )
+    );
+    contractors.push(
+        new Contractor(
+            faker.random.number(),
+            faker.name.firstName(),
+            faker.name.firstName(),
+            faker.name.lastName(),
+            faker.date.past(),
+            faker.phone.phoneNumber(),
+            faker.internet.email(),
+            faker.address.streetAddress(),
+            faker.address.city(),
+            faker.address.state(),
+            faker.address.zipCode(),
+            faker.company.companyName()
+        )
+    );
+    managers.push(
+        new Manager(
+            faker.random.number(),
+            faker.random.number(),
+            faker.name.firstName(),
+            faker.name.firstName(),
+            faker.name.lastName(),
+            faker.date.past(),
+            faker.phone.phoneNumber(),
+            faker.internet.email(),
+            faker.address.streetAddress(),
+            faker.address.city(),
+            faker.address.state(),
+            faker.address.zipCode(),
+            faker.random.number(),
+            faker.commerce.department(),
+            faker.name.jobTitle(),
+            faker.finance.amount(),
+            faker.random.number(),
+            faker.random.number()
+        )
+    );
+    if (i < 3) {
+        executives.push(
+            new Executive(
+                faker.random.number(),
+                faker.name.firstName(),
+                faker.name.firstName(),
+                faker.name.lastName(),
+                faker.date.past(),
+                faker.phone.phoneNumber(),
+                faker.internet.email(),
+                faker.address.streetAddress(),
+                faker.address.city(),
+                faker.address.state(),
+                faker.address.zipCode(),
+                faker.random.number(),
+                faker.commerce.department(),
+                faker.name.jobTitle(),
+                faker.finance.amount(),
+                faker.random.number(),
+                faker.finance.amount(),
+                faker.random.number()
+            )
+        );
     }
 }
-for (let i=0; i<20; i++){
-    employees.push(new Employee(faker.random.number(), faker.random.number(), faker.name.firstName(), faker.name.firstName(), faker.name.lastName(), faker.date.past(), faker.phone.phoneNumber(), faker.internet.email(), faker.address.streetAddress(), faker.address.city(), faker.address.state(), faker.address.zipCode(), faker.random.number(), faker.commerce.department(), faker.name.jobTitle(), faker.finance.amount(), faker.random.number()))
+for (let i = 0; i < 20; i++) {
+    employees.push(
+        new Employee(
+            faker.random.number(),
+            faker.random.number(),
+            faker.name.firstName(),
+            faker.name.firstName(),
+            faker.name.lastName(),
+            faker.date.past(),
+            faker.phone.phoneNumber(),
+            faker.internet.email(),
+            faker.address.streetAddress(),
+            faker.address.city(),
+            faker.address.state(),
+            faker.address.zipCode(),
+            faker.random.number(),
+            faker.commerce.department(),
+            faker.name.jobTitle(),
+            faker.finance.amount(),
+            faker.random.number()
+        )
+    );
 }
-
 
 // 4 - modify the objects
 for (employee of employees) {
-    employee.managerId = managers[Math.floor(Math.random() * managers.length)].id
+    employee.managerId =
+        managers[Math.floor(Math.random() * managers.length)].id;
 }
-managers[0].managerId = executives[0].id
-managers[1].managerId = executives[0].id
+managers[0].managerId = executives[0].id;
+managers[1].managerId = executives[0].id;
 
-managers[2].managerId = executives[1].id
-managers[3].managerId = executives[1].id
-managers[4].managerId = executives[1].id
+managers[2].managerId = executives[1].id;
+managers[3].managerId = executives[1].id;
+managers[4].managerId = executives[1].id;
 
-executives[0].managerId = executives[2].id
-executives[1].managerId = executives[2].id
+executives[0].managerId = executives[2].id;
+executives[1].managerId = executives[2].id;
 
-executives[2].managerId = executives[2].id
+executives[2].managerId = executives[2].id;
+
+let db = new sqlite.Database("sqlite.db", (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log("\nconnected to db");
+});
 
 // console.log(vendors)
 // console.log(customers)
@@ -114,9 +446,8 @@ executives[2].managerId = executives[2].id
 // console.log(executives)
 // console.log(employees)
 
-
-// ORM 2 Employee testing - - - - - - - - 
-// employee2DAO.create(employees[0]) 
+// ORM 2 Employee testing - - - - - - - -
+// employee2DAO.create(employees[0])
 // employee2DAO.read(employees[0].id)
 // employees[0].firstName = 'Connor'
 // employee2DAO.update(employees[0])
@@ -125,3 +456,18 @@ executives[2].managerId = executives[2].id
 // for (employee of employees) {
 //     employee2DAO.create(employee)
 // }
+
+// ORM 2 customer testing - - - - - - - -
+customer2DAO.create(customers[0], db);
+customer2DAO.read(customers[0].id, db);
+customers[0].firstName = "Connor";
+customer2DAO.update(employees[0]);
+customer2DAO.remove(73627);
+customer2DAO.list();
+
+db.close((err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log("closing sqlite database\n");
+});
--- sqliteDAO/ORM2/customerDAO2Sqlite.js
+++ sqliteDAO/ORM2/customerDAO2Sqlite.js
@@ -1,25 +1,183 @@
-// ORM 1 sqlite customers
+// ORM 2 sqlite customers
+// const sqlite = require("sqlite3").verbose();
+// const faker = require("faker");
 
-create = (customer) => {
+const create = (customer, db) => {
+    db.serialize(() => {
+        db.run(
+            `INSERT INTO orm2_customer ( id, "firstName", "middleName", "lastName", dob, phone, email, "streetAddress", city, state, zip, company) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)`,
+            [
+                customer.id,
+                customer.firstName,
+                customer.middleName,
+                customer.lastName,
+                customer.dob,
+                customer.phone,
+                customer.email,
+                customer.streetAddress,
+                customer.city,
+                customer.state,
+                customer.zip,
+                customer.company,
+            ],
+            function (err) {
+                if (err) {
+                    return console.log(err.message);
+                }
+                // get the last insert id
+                console.log(`Success, created customer`);
+            }
+        );
+    });
+};
+
+const read = (id, db) => {
+    let sql = `SELECT * FROM orm2_customer WHERE id = ${id}`;
+    db.serialize(() => {
+        db.get(sql, [], (err, row) => {
+            if (err) {
+                return console.error(err.message);
+            }
+            let obj = {
+                id: row.id,
+                firstName: row.firstName,
+                middleName: row.middleName,
+                lastName: row.lastName,
+                dob: row.dob,
+                phone: row.phone,
+                email: row.email,
+                streetAddress: row.streetAddress,
+                city: row.city,
+                state: row.state,
+                zip: row.zip,
+                company: row.company,
+            };
+            // {object}
+            return row
+                ? console.log(obj)
+                : console.log(`No record found with the id ${id}`);
+        });
+    });
+};
+
+const update = (customer, db) => {
+    // let db = new sqlite.Database("sqlite.db", (err) => {
+    //     if (err) {
+    //         console.error(err.message);
+    //     }
+    //     console.log("\nconnected to db");
+    // });
+    let data = [
+        customer.id,
+        customer.firstName,
+        customer.middleName,
+        customer.lastName,
+        customer.dob,
+        customer.phone,
+        customer.email,
+        customer.streetAddress,
+        customer.city,
+        customer.state,
+        customer.zip,
+        customer.companyId,
+        customer.department,
+        customer.title,
+        customer.salary,
+        customer.managerId,
+        customer.id,
+    ];
+
+    db.run(
+        `UPDATE orm2_customer SET 
+        id = ?, 
+        "firstName" = ?, 
+        "middleName" = ?, 
+        "lastName" = ?,
+        dob = ?,
+        phone = ?, 
+        email = ?, 
+        "streetAddress" = ?, 
+        city = ?, 
+        state = ?, 
+        zip = ?, 
+        "companyId" = ?, 
+        department = ?, 
+        title = ?, 
+        salary = ?, 
+        "managerId" = ?
+        WHERE id = ?;`,
+        data,
+        function (err) {
+            if (err) {
+                return console.log(err.message);
+            }
+            // get the last insert id
+            console.log(`Success, updated customer`);
+        }
+    );
+
+    db.close((err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log("closing sqlite database\n");
+    });
     // no return
-}
+};
 
-read = (id) => {
-    // return vendor object
-}
+const remove = (id, db) => {
+    // let db = new sqlite.Database("sqlite.db", (err) => {
+    //     if (err) {
+    //         console.error(err.message);
+    //     }
+    //     console.log("\nconnected to db");
+    // });
 
-update = (customer) => {
-    // replace existing row(s)
+    let sql = `DELETE FROM orm2_customer WHERE id = ${id}`;
+    db.run(sql, (err, row) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        // {object}
+        return row
+            ? console.log(`No record found with the id ${id}`)
+            : console.log(`record ${id} removed from DB`);
+    });
+
+    db.close((err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log("closing sqlite database\n");
+    });
     // no return
-}
+};
 
-remove = (id) => {
-    // no return
-}
+const list = (db) => {
+    // let db = new sqlite.Database("sqlite.db", (err) => {
+    //     if (err) {
+    //         console.error(err.message);
+    //     }
+    //     console.log("\nconnected to db");
+    // });
 
-list = () => {
-    // return array of objects
-}
+    let sql = `SELECT * FROM orm2_customer;`;
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            console.error(err.message);
+        }
+        // return array
+        rows.forEach((row) => {
+            console.log(row);
+        });
+    });
 
+    db.close((err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log("closing sqlite database\n");
+    });
+};
 
-module.exports = {create, read, update, remove, list}
+module.exports = { create, read, update, remove, list };
